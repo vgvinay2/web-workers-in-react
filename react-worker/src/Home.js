b05dea03baf9a48f460fb76a71ba4db69e4a8eb8
Vinay Gupta <vinay.gupta@rakuten.com>
2020-09-21 12:05:19 +0530

    worker commit

---
+++ react-worker/src/Home.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import ReactCountdownClock from "react-countdown-clock";
+import worker from "./worker.js";
+import WebWorker from "./workerSetup";
+import "./App.css";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  fetchUsers = () => {
+    const users = [];
+
+    const userDetails = {
+      name: "Jane Doe",
+      email: "jane.doe@gmail.com",
+      id: 1
+    };
+
+    for (let i = 0; i < 10000000; i++) {
+      userDetails.id = i++;
+      userDetails.dateJoined = Date.now();
+
+      users.push(userDetails);
+    }
+
+    this.setState({
+      count: users.length
+    });
+  };
+
+  fetchWebWorker = (props) => {
+    console.log('fetaching from web worker', this.worker);
+    this.worker.postMessage("Fetch Users", this.worker);
+     // console.log('before click', this.worker)
+    this.worker.addEventListener("message", event => {
+      console.log('after clcik', event.data)
+      this.setState({
+        count: event.data.length
+      });
+      console.log('After click', event);
+    });
+  };
+
+  componentDidMount = () => {
+    this.worker = new WebWorker(worker);
+  };
+
+  render() {
+    return (
+      <div className="App-bottom">
+        <section className="App-left">
+          <ReactCountdownClock
+            seconds={100}
+            color="#000"
+            alpha={0.9}
+            size={300}
+          />
+          <p className="text-center">Total User Count: {this.state.count}</p>
+          <button className="btn-direct" onClick={this.fetchUsers}>
+            Fetch Users Directly
+          </button>
+        </section>
+
+        <section className="App-right">
+          <ReactCountdownClock
+            seconds={100}
+            color="#e56"
+            alpha={0.9}
+            size={300}
+          />
+          <p className="text-center">Total User Count: {this.state.count}</p>
+          <button className="btn-worker" onClick={this.fetchWebWorker}>
+            Fetch Users with Web Worker
+          </button>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Home;
